e43a041f2a76ac6a62aa7707e7032755839f1235
lamterm <luis.morandi@coderhouse.com>
2024-07-26 02:01:38 -0300

    role test

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
+import { now } from "mongoose";
 import app, { port } from "./app";
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port} - ${new Date()}`);
 });
